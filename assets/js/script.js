9c449168505a89ec604a292e77240cb6e95162b7
JAKHONGIR DARMANOV <97786628+darmanovjakhongir@users.noreply.github.com>
2024-09-22 22:44:58 +0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const video = document.getElementById('video');
+const playBtn = document.getElementById('play-btn');
+
+playBtn.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+    playBtn.innerHTML = '<img src="./assets/images/pause-circle.svg" alt="Pause Icon">';
+  } else {
+    video.pause();
+    playBtn.innerHTML = '<img src="./assets/images/stop-circle.svg" alt="Stop Icon">';
+  }
+});
